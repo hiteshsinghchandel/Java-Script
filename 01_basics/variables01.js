8ee1ccb4520f6d3a1b67519ead0f5731e067ed5f
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2025-07-14 18:11:00 +0000

    variable related

---
+++ 01_basics/variables01.js
@@ -0,0 +1,13 @@
+const name = "Bharat Singh";
+// can not change the value in const keyword after asigning...
+let City = "jaipur"
+var state = "rajasthan"
+flat = 301     /*
+we can write in this format but this is not a good practice */
+
+
+let  college ;
+// do not prefer var because of scope problem....
+
+
+console.table([name,City,state,college ,flat]);
