a18b44e70ededfda7365c73d58eadad1ab0d409a
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2025-07-15 16:43:05 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict";   //this is used to treat the code as new version of java script
+
+
+/* types of datatype 
+1 number = 2^53 stoere
+2 bigint
+string =" " use in " " omly
+boolen = true /flase values only
+null  = a value */
+
+console.log(typeof null);  // it is a object
+console.log(typeof undefined);// its type is undefind.....
+
+
+
+
+let score = 95
+
+console.log(typeof(score));
