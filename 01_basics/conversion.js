eb1644013bb0a269fb1b4e6f362493fd3792762d
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2025-07-17 16:58:54 +0000

    conversion

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,1 +1,13 @@
-let marks = 95;
+let marks = "abs7";
+
+console.log(typeof(marks))
+
+let m = Number(marks)
+let t = String(marks)
+console.log(typeof(m))
+console.log((m))
+// NaN stand for not a number ....
+// we can convert a data type to another 
+// syntax:
+// let name = Datatype{String /Number}(the variable which we want to convert).............
+
