361c58ce077ebb04128e408e464f4f9a2c13a5f4
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2025-07-18 06:24:31 +0000

    about some operators

---
+++ 01_basics/operators.js
@@ -0,0 +1,20 @@
+/* there are many operators in js 
+1. < or > these are for comparison 
+2. <= or >= these are also for comparison but with equility
+3. ==  this is used when the value is same 
+4. === this is also used when value is same also check the type of data
+5. != not equal to 
+6. !==  this for strict not equal same as 4th.....
+*/
+let m = 15 > 13
+console.log(m);
+
+let n = (15 === '15')
+console.log(n);
+
+let p = (15 <= "15")
+console.log(p);
+
+
+// these are some example about operators 
+
