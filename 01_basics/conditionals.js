7263081024b60e53b5dc6e3a6d9044c534437876
Bharat singh <145591613+hiteshsinghchandel@users.noreply.github.com>
2025-07-21 15:19:15 +0000

    conditionals

---
+++ 01_basics/conditionals.js
@@ -0,0 +1,40 @@
+/* in js there are 
+1. if -else statements...
+2. switch statements.....
+the use of these statements is same as in other languages like c++ ,java etc...
+
+*/
+
+
+let a = "boy"
+
+if(a ==="boy"){
+    console.log("its working ");
+    
+}else{
+    console.log("not worling....")
+}
+// use of switch.....
+
+let day = 5;
+
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday")
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thusday");
+        break;
+    case 5:
+        console.log("friday");
+        break ;
+    case 6:
+        console.log("saturday");
+        break;      
+}
